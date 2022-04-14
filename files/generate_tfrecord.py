957516959a279d7596668d8d08ec5fbd150ad38e
zahraahnd <87270138+zahraahnd@users.noreply.github.com>
2022-04-14 14:17:38 +0700

    Update generate_tfrecord.py

--- files/generate_tfrecord.py
+++ files/generate_tfrecord.py
@@ -81,16 +81,16 @@ def xml_to_csv(path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
-        value = (root.find('filename').text,
-                     int(root.find('size').find('width').text),
-                     int(root.find('size').find('height').text),
-                     member[0].text,
-                     int(member.find("bndbox").find('xmin').text),
-                     int(member.find("bndbox").find('ymin').text),
-                     int(member.find("bndbox").find('xmax').text),
-                     int(member.find("bndbox").find('ymax').text)
-                    )
-            xml_list.append(value)
+          value = (root.find('filename').text,
+                       int(root.find('size').find('width').text),
+                       int(root.find('size').find('height').text),
+                       member[0].text,
+                       int(member.find("bndbox").find('xmin').text),
+                       int(member.find("bndbox").find('ymin').text),
+                       int(member.find("bndbox").find('xmax').text),
+                       int(member.find("bndbox").find('ymax').text)
+                      )
+          xml_list.append(value)
     column_name = ['filename', 'width', 'height',
                    'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
